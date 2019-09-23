49a37c94de871a3cef96eda5af728c04235368c2
jannick holm <jannickholm@desktop-f97g8mt.kea.local>
2019-09-23 12:32:52 +0200

    Adds post function

--- index.js
+++ index.js
@@ -9,16 +9,41 @@ function get() {
         })
         .then(e => e.json())
         .then(friends => {
-
-            friends.forEach(friend => {
-                const template = document.querySelector("template").content;
-                const copy = template.cloneNode(true);
-                copy.querySelector("h1").textContent = friend.name;
-                copy.querySelector("h4").textContent = friend.email;
-                copy.querySelector("p").textContent = friend.age;
-                document.querySelector("#app").appendChild(copy);
-            });
+            friends.forEach(addFriend);
         });
+    document.querySelector('button').addEventListener('click', post);
+}
+
+function addFriend(friend) {
+    const template = document.querySelector("template").content;
+    const copy = template.cloneNode(true);
+    copy.querySelector("h1").textContent = friend.name;
+    copy.querySelector("h4").textContent = friend.email;
+    copy.querySelector("p").textContent = friend.age;
+    document.querySelector("#app").appendChild(copy);
 }
 
 get();
+
+function post() {
+    const data = {
+        name: "Jannick Holm",
+        email: "hi@jannickholm.dk",
+        age: 21
+    };
+    const postData = JSON.stringify(data);
+    fetch('https://frontendautmn2019-15d9.restdb.io/rest/friends', {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json; charset=utf-8',
+                'x-apikey': '5d887445fd86cb75861e25f4',
+                'cache-control': 'no-cache'
+            },
+            body: postData
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            addFriend(data);
+        });
+}
